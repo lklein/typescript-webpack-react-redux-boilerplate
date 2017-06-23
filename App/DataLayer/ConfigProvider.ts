619f0a4b4692b2d0b6e8371ad0c6bf13e40a8002
lklein <lacusklein2013@gmail.com>
2017-06-23 11:38:21 -0700

    Added mock server

--- App/DataLayer/ConfigProvider.ts
+++ App/DataLayer/ConfigProvider.ts
@@ -1,7 +1,8 @@
 // Handle errors in real world app
 export function fetchConfig(section: string): Promise<string> {
 
-    const url = `http://localhost:27830/api/mds/${section}/`;
+    const url = `http://localhost:3004/${section}/`;
+    // const url = `http://localhost:27830/api/mds/${section}/`;
 
     return fetch(url)
         .then((response: Response) => response.json())
